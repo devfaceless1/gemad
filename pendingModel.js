ee327f1f50fef394cb28d07d5c191a048ea4213f
BuildTools <unconfigured@null.spigotmc.org>
2025-10-14 19:31:55 +0200

    Fix

--- pendingModel.js
+++ pendingModel.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 
 const pendingSchema = new mongoose.Schema({
-  telegramId: { type: String, required: true },
-  channel: { type: String, required: true },
-  checkAfter: { type: Date, required: true },
-  status: { type: String, default: 'waiting' }
-});
+    telegramId: { type: String, required: true },
+    channel: { type: String, required: true },
+    reward: { type: Number, required: true },
+    status: { type: String, default: "waiting" },
+    checkAfter: { type: Date, required: true }
+}, { timestamps: true });
 
-export const PendingSub = mongoose.model('PendingSub', pendingSchema);
+export const Pending = mongoose.model('Pending', pendingSchema);
