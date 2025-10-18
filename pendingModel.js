ffa393367322e2e5965323b56829c48703ef6f95
BuildTools <unconfigured@null.spigotmc.org>
2025-10-18 12:08:08 +0200

    initial commit

--- pendingModel.js
+++ pendingModel.js
@@ -1,9 +1,12 @@
+import mongoose from 'mongoose';
+
 const pendingSchema = new mongoose.Schema({
   telegramId: { type: String, required: true },
   channel: { type: String, required: true },
   reward: { type: Number, required: true },
-  status: { type: String, default: "waiting" }, 
-  checkAfter: { type: Date, required: true },
-  error: { type: String, default: null },       
-  processedAt: { type: Date, default: null }    
+  status: { type: String, default: "waiting" },
+  checkAfter: { type: Date, required: true }
 }, { timestamps: true });
+
+
+export default mongoose.model('Pending', pendingSchema);
